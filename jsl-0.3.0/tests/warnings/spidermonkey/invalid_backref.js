51331b0420faed8a0e98af033a09f039ad2b2786
unasm <douunasm@gmail.com>
2012-11-19 00:37:49 +0800

    删除了一些不需要的，和下载的压缩文件

--- jsl-0.3.0/tests/warnings/spidermonkey/invalid_backref.js
+++
@@ -1,5 +0,0 @@
-/*jsl:option explicit*/
-function invalid_backref() {
-    /* illegal - \0 is not a valid regex backreference */
-    var re = /\0/; /*warning:invalid_backref*/
-}
--- jsl-0.3.0/tests/warnings/var_hides_arg.js
+++
@@ -1,4 +0,0 @@
-/*jsl:option explicit*/
-function var_hides_arg(duplicate) {
-    var duplicate; /*warning:var_hides_arg*/
-}
